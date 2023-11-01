c9571b53ccd37e9158750d0731475dea1d1b2b8c
Fabián Abarca <ensinergia@gmail.com>
2023-11-01 10:08:14 -0300

    Revisión de formulario de creación de ruta

--- editing/urls.py
+++ editing/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('', views.edition, name='edition'),
     path('listo/', views.edited, name='edited'),
     path('agencia/', views.agency, name='agency'),
+    path('rutas/crear', views.create_route, name='create_route'),
+    path('rutas/<route_id>/', views.edit_route, name='edit_route'),
+    # path('rutas/<route_id>/', views.route, name='route')
 ]
--- editing/views.py
+++ editing/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
-from gtfs.models import Agency
-from gtfs.models import Route
+from gtfs.models import Agency, Route
 
 # Create your views here.
 
@@ -26,23 +25,37 @@ def agency(request):
     else:
         return render(request, "agency.html")
 
-def routes(request):
+
+def create_route(request):
     if request.method == "POST":
-        routes = Route(
+        agency = Agency.objects.get(agency_id=request.POST["route_agency"])
+        route = Route(
             route_id=request.POST["route_id"],
-            agency=request.POST["route_agency"],
+            agency=agency,
             short_name=request.POST["route_short_name"],
             long_name=request.POST["route_long_name"],
             desc=request.POST["route_desc"],
             route_type=request.POST["route_type"],
             url=request.POST["route_url"],
-            color=request.POST["route_color"],
-            text_color=request.POST["route_text_color"],
+            color=request.POST["route_color"][1:7],
+            text_color=request.POST["route_text_color"][1:7],
         )
-        agency.save()
+        route.save()
         return redirect("edited")
     else:
-        return render(request, "routes.html")
+        agencies = Agency.objects.all()
+        route_type_choices = Route.ROUTE_TYPE_CHOICES
+        context = {
+            "agencies": agencies,
+            "route_type_choices": route_type_choices,
+        }
+        return render(request, "routes.html", context)
+
+
+def edit_route(request, route_id):
+    route = Route.objects.get(route_id=route_id)
+    pass
+
 
 def edited(request):
     return render(request, "edited.html")
